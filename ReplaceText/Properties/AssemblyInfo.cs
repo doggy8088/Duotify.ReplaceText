409312ff169542960af44dbbae6c1736b0064727
Will <doggy.huang@gmail.com>
2025-10-08 21:58:11 +0800

    chore: 更新版本號至 1.0.1，調整 AssemblyInfo 與專案檔案中的版本資訊

--- ReplaceText/Properties/AssemblyInfo.cs
+++ ReplaceText/Properties/AssemblyInfo.cs
@@ -8,9 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ReplaceText")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("多奇數位創意有限公司")]
-[assembly: AssemblyProduct("ReplaceText")]
-[assembly: AssemblyCopyright("Copyright © Will 保哥 2010-2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +28,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("1.0.0.0")]
+// [assembly: AssemblyFileVersion("1.0.0.0")]
